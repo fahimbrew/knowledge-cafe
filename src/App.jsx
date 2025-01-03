0d5b8319082b643c0a52689e665165e4e352f13f
fahimbrew <iftiaqhossain@gmail.com>
2025-01-03 12:36:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Blogs from "./Components/Blogs/Blogs"
+import Header from "./Components/Header/Header"
+
+function App() {
+  
+
+  return (
+    <>
+     <Header></Header>
+     <Blogs></Blogs>
+     
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-semibold">Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
